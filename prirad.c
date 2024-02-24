3d89c1cf9803bc2a205b339a3ac6b453d7c488fa
hannahkuklovska <hannah15072002@gmail.com>
2024-02-24 13:10:39 +0100

    pretypovanie hodnoty

--- prirad.c
+++ prirad.c
@@ -6,8 +6,10 @@ int main()
      unsigned short int h = 300;
      unsigned char ch;
 
-     ch = h;
+     ch = (unsigned char)h;
 
      printf("Hodnota pred: %u \n", h);
      printf("Hodnota po: %u \n", ch);
+
+     return 0;
 }
