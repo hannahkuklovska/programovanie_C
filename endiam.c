859352cc5dc3b2f906b5d0a0413ca2d9a29f26bf
hannahkuklovska <hannah15072002@gmail.com>
2024-02-24 08:49:55 +0100

    porovnanie

--- endiam.c
+++ endiam.c
@@ -4,9 +4,11 @@ int main()
 {
 
      unsigned int e = 1;
+     unsigned int n = 1000;
 
      // ale chcem vedieť jeho adresu, nie hodnotu (per se)
-     printf("Hodnota je: %p \n", &e);
+     printf("Adresa %u je: %p \n", e, &e);
+     printf("Adresa %u je: %p \n", n, &n);
 
      return 0;
 }
