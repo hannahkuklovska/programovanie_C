f9e04829209be04514f5762d229516f8a1ec8e31
hannahkuklovska <hannah15072002@gmail.com>
2024-02-26 21:45:16 +0100

    cez decimalnu (pre pocitac binarnu)

--- endiam.c
+++ endiam.c
@@ -2,16 +2,16 @@
 
 int main(void)
 {
-     unsigned int e = 3;      // 00 00 00 11 (binarna sustava)
-     char *e_ad = (char *)&e; // pointer na adresu e, ale e uz teraz pretypovane na char
+     unsigned int e = 3; // 00 00 00 11 (binarna sustava)
+     // resp. 00000000 00000000 00000000 00000011 (3 v 32bitoch)
+     char *e_ad = (char *)&e; // pointer na adresu e, ale e uz teraz pretypovane na char, e_ad ukazuje na prvy bajt pamate, kde je ulozene e
+     // na char, aby sme to mohli citat po bajte
 
-     printf("%x\n", e_ad);
-
-     if (*e_ad == 3)
+     if (*e_ad == 3) // je hodnota prveho bajtu 3? //00 00 00 11
      {
           printf("Little endian.\n");
      }
-     else if (*e_ad == 0)
+     else if (*e_ad == 0) // 00 00 00 00
      {
           printf("Big endian.\n");
      }
