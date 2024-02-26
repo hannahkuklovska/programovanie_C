45a53f2bdbeb8a9d7f5a70546bb66d9331977747
hannahkuklovska <hannah15072002@gmail.com>
2024-02-26 21:00:53 +0100

    rozne zapisy

--- endiam.c
+++ endiam.c
@@ -2,9 +2,11 @@
 
 int main(void)
 {
-     unsigned int e = 0b00000011;
+     unsigned int e = 0b101010100000000011111111;
      unsigned char *e_ad = (unsigned char *)&e;
-     if (*e_ad == 0b11)
+
+     printf("Hexadecimal representation bajtu je: %x\n", *e_ad);
+     if (*e_ad == 0b11111111)
      {
           printf("Little endian.\n");
      }
