363eabdeddb5731ff66dbe91fe1f1ad6ac0dab26
hannahkuklovska <hannah15072002@gmail.com>
2024-02-26 16:12:23 +0100

    hranie sa s adresov a hodnotou

--- prirad.c
+++ prirad.c
@@ -3,15 +3,19 @@
 int main()
 {
 
-     unsigned short int h = 300;
-     unsigned char ch;
+     unsigned short int h = 300; // 16 bitov
+     unsigned char ch;           // 8 bitov
 
-     ch = (unsigned char)h;
+     ch = (unsigned char)h; // pretypovanie
 
      printf("Hodnota pred: %u \n", h);
+     printf("Adresa pred: %p \n", &h);
      printf("Hodnota po: %u \n", ch);
+     printf("Adresa po: %p \n", &ch);
 
      return 0;
 
-     // doslo k strate hodnoty, ulozila sa mensia hodnota, strata 300, po pretypovani vyjde 44
+     // doslo k strate hodnoty, ulozila sa mensia hodnota, strata 300, po pretypovani vyjde 44 (300-256)
+     // zostala iba hodnota 8 bitov (least significant)
+     // cita tym padom z prava do lava (?)
 }
