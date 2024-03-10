561d684e5c529ec65e8aa7727b0b490f4f045f6d
hannahkuklovska <hannah15072002@gmail.com>
2024-03-10 22:03:15 +0100

    začiatok, prvá podmienka

---
+++ mta_odmocnina.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int mta_odmocnina(unsigned int m, double x);
+
+int main(void)
+{
+}
+
+int mta_odmocnina(unsigned int m, double x)
+{
+     if (m == 0)
+     {
+          printf("Takto to bohužiaľ nepôjde.\n");
+     }
+}
