96cc2f23d5167d750a8d5368a2c65f36a0103267
hannahkuklovska <hannah15072002@gmail.com>
2024-03-10 22:55:21 +0100

    nie je to ešte ono

--- mta_odmocnina.c
+++ mta_odmocnina.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 
-double mta_odmocnina(unsigned int m, double x);
+double mta_odmocnina(int m, double x);
 double mocnina(double mocnenec, int exponent);
 
 int main(void)
 {
+     int m = 2;
+     double x = 8;
+
+     double vysledok = mta_odmocnina(m, x);
+     printf("%lf\n", vysledok);
+
+     return 0;
 }
 
-double mta_odmocnina(unsigned int m, double x)
+double mta_odmocnina(int m, double x)
 {
      if (m == 0 || (x < 0 && m % 2 == 0))
      {
           return 1;
      }
 
-     int vysledok;
-     for (int i = 0; i < m; i++)
-     {
-          vysledok = mocnina(x, 1 / m);
-     }
+     double vysledok = mocnina(x, 1.0 / m);
      return vysledok;
 }
 
 double mocnina(double mocnenec, int exponent)
 {
-     double vysledok = 1;
+     double vysledok = 1.0;
      for (int i = 0; i < exponent; i++)
      {
           vysledok *= mocnenec;
