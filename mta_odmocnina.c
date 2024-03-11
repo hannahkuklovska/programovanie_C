7024dd8cec32942fbb46f13cfd91ac50bc9f6785
hannahkuklovska <hannah15072002@gmail.com>
2024-03-11 04:26:40 +0100

    prerobenie

--- mta_odmocnina.c
+++ mta_odmocnina.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
-#include <math.h>
 
 double mta_odmocnina(int m, double x);
 double mocnina(double mocnenec, double exponent);
 
 int main(void)
 {
-     int m = 2;
-     double x = 8;
+     int m, x;
 
-     double vysledok = mta_odmocnina(m, x);
-     printf("%lf\n", vysledok);
+     printf("Zadajte cislo: ");
+     scanf("%d", &x);
 
-     printf("%lf\n", mocnina(x, 1.0 / m));
+     printf("Zadajte odmocninu (m): ");
+     scanf("%d", &m);
+
+     if (m < 0)
+     {
+          printf("Chyba: Odmocnina zo zaporneho cisla.\n");
+          return -1;
+     }
+
+     printf("%dha odmocnina z %d je: %lf\n", m, x, mocnina(m, 1.0 / x));
+
+     return 0;
 }
 
 double mta_odmocnina(int m, double x)
 {
-     if (m == 0 || (x < 0 && m % 2 == 0))
+     if (m == 0)
      {
-          return 1;
+          printf("Chyba: Vypocet nultej odmocniny.\n");
+          return -1;
+     }
+
+     if (x < 0 && m % 2 == 0)
+     {
+          printf("Chyba: Vypocet parnej odmocniny zo zaporneho cisla.\n");
+          return -1;
      }
 
      double vysledok = mocnina(x, 1.0 / m);
@@ -28,10 +44,20 @@ double mta_odmocnina(int m, double x)
 
 double mocnina(double mocnenec, double exponent)
 {
+     if (exponent == 0)
+     {
+          return -1;
+     }
+
+     if (mocnenec == 0)
+          return 0;
+
      double vysledok = 1.0;
+
      for (int i = 0; i < exponent; i++)
      {
           vysledok *= mocnenec;
      }
+
      return vysledok;
 }
