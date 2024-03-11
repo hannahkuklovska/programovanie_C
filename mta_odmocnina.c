aaf8a4b2ce4de72e5331d285290d176409508169
hannahkuklovska <hannah15072002@gmail.com>
2024-03-11 22:03:23 +0100

    snaha o zlepšenie

--- mta_odmocnina.c
+++ mta_odmocnina.c
@@ -2,6 +2,7 @@
 
 #define tol 0.0001
 #define abs(n) (((n) < 0) ? -(n) : (n))
+#define hod 100000
 
 double mta_odmocnina(int m, double x);
 double mocnina(double mocnenec, double exponent);
@@ -22,7 +23,7 @@ int main(void)
           return -1;
      }
 
-     printf("%dha odmocnina z %d je: %lf\n", m, x, mocnina(m, 1.0 / x));
+     printf("%dta odmocnina z %d je: %lf\n", m, x, mta_odmocnina(m, x));
 
      return 0;
 }
@@ -32,17 +33,16 @@ double mta_odmocnina(int m, double x)
      if (m == 0)
      {
           printf("Chyba: Vypocet nultej odmocniny.\n");
-          return -1;
+          return 1;
      }
 
      if (x < 0 && m % 2 == 0)
      {
           printf("Chyba: Vypocet parnej odmocniny zo zaporneho cisla.\n");
-          return -1;
+          return 1;
      }
 
      double pomoc = 1.0;
-     int i;
 
      for (int i = 0; i < m; i++)
      {
@@ -52,14 +52,20 @@ double mta_odmocnina(int m, double x)
           }
           pomoc += tol;
      }
-     // while (abs(m / pomoc - pomoc) > tol)
+     int i = hod;
+     while (i > 0 && abs(mocnina(pomoc, m) - x) > tol)
+     {
+          pomoc -= tol;
+          i--;
+     }
+     return pomoc;
 }
 
 double mocnina(double mocnenec, double exponent)
 {
      if (exponent == 0)
      {
-          return -1;
+          return 1;
      }
 
      if (mocnenec == 0)
